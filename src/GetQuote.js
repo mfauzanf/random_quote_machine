1256be93bc6331f740e525933c8308d2a1d8aa85
Muhammad Fauzan Fikri <mfauzanfff@gmail.com>
2020-08-10 10:58:11 +0700

    add rel noopener noreferrer

--- src/GetQuote.js
+++ src/GetQuote.js
@@ -44,7 +44,7 @@ class GetQuote extends React.Component {
           - <span id="author">{this.state.author}</span>
         </div>
         <div className="buttons">
-          <a className="button" id="tweet-quote" title="Tweet this quote!" target="_blank" href={this.state.url}>
+          <a className="button" id="tweet-quote" title="Tweet this quote!" target="_blank" rel="noopener noreferrer" href={this.state.url}>
             <i className="fa fa-twitter"></i>
           </a>
           <button className="button" id="new-quote" onClick={this.quote}>New quote</button>
